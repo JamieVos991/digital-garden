517218995b3c2be0055e33329d0aed24b08de8a1
Jamie Vos <Jamievos100@gmail.com>
2025-11-10 16:37:54 +0100

    Javascript toegevoegd, de knoppen werken nu voor de sprints 🔨

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const knoppen = document.querySelectorAll("button[data-sprint]");
+    const sprints = document.querySelectorAll(".sprint");
+  
+    knoppen.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const index = btn.dataset.sprint;
+  
+        sprints.forEach((s) => s.classList.remove("active"));
+  
+        if (sprints[index]) {
+          sprints[index].classList.add("active");
+        }
+      });
+    });
+  
+    if (sprints.length > 0) {
+      sprints[0].classList.add("active");
+    }
+  });
+  
